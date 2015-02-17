385ec69f9bacb355cddf80e45a7fe35db3a3edab
Yoshiyuki Taniguchi <iwatesoftware@live.jp>
2015-02-17 09:27:57 +0900

    initial commit

---
+++ FxOSHelper/FxOSCorsPolicyFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Web.Http.Cors;
+
+namespace FxOSHelper
+{
+    public class FxOSCorsPolicyFactory : ICorsPolicyProviderFactory
+    {
+        ICorsPolicyProvider provider;
+        public FxOSCorsPolicyFactory(string[] allowOrigins)
+        {
+            provider = new FxOSCorsPolicyProvider(allowOrigins);
+        }
+        public FxOSCorsPolicyFactory(string allowOrigin)
+        {
+             provider = new FxOSCorsPolicyProvider(allowOrigin);
+        }
+        public ICorsPolicyProvider GetCorsPolicyProvider(HttpRequestMessage request)
+        {
+            return provider;
+        }
+    }
+}
---
+++ FxOSHelper/FxOSCorsPolicyProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http.Cors;
+
+namespace FxOSHelper
+{
+    public class FxOSCorsPolicyProvider : ICorsPolicyProvider
+    {
+        private string[] origins;
+        public FxOSCorsPolicyProvider(string[] allowOrigins)
+        {
+            origins = allowOrigins;
+        }
+        public FxOSCorsPolicyProvider(string allowOrigin)
+        {
+            origins = new string[] { allowOrigin };
+        }
+        public Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Create a CORS policy.
+            var policy = new CorsPolicy
+            {
+                AllowAnyMethod = true,
+                AllowAnyHeader = true
+            };
+
+            // Add allowed origins.
+            foreach (string origin in origins)
+            {
+                policy.Origins.Add(origin);
+            }
+            return Task.FromResult(policy);
+        }
+    }
+}
